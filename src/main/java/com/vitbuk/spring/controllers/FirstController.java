0905bba90aaf2517f0cf26359d0bc3abbaa3a623
VitBuk <vitbuk@gmail.com>
2023-02-07 12:46:18 +0200

    calculatePage addede

--- src/main/java/com/vitbuk/spring/controllers/FirstController.java
+++ src/main/java/com/vitbuk/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.vitbuk.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +14,45 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam("name") String name,
-                            @RequestParam("surname") String surname) {
-        System.out.println("Hello, " + name + " " + surname );
+                            @RequestParam("surname") String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatePage(@RequestParam("a") int a,
+                                @RequestParam("b") int b,
+                                @RequestParam("action") String action,
+                                Model model){
+        String answer = "Result is: ";
+        System.out.println("Action: " + action);
+
+        switch (action) {
+            case "addition":
+                answer += String.valueOf(a + b);
+                break;
+            case "subtraction":
+                answer += String.valueOf(a - b);
+                break;
+            case "multiplication":
+                answer += String.valueOf(a * b);
+                break;
+            case "division":
+                answer += String.valueOf(a / b);
+                break;
+            default:
+                answer = "There is no such operation, please use: addition, subtraction, multiplication or division ";
+                break;
+        }
+
+        model.addAttribute("answer", answer);
+        return "first/calculated";
     }
 
 
